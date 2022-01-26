eaaf3d78bf4fa25e1fe4da89e5516109b3c7d4d5
phonichail <olebjogra@gmail.com>
2022-01-27 00:47:54 +0100

    added first part of task3

---
+++ module1/model.md
@@ -0,0 +1,6 @@
+1 Menneske
+2 Kylling
+3 Korn
+4 Rev
+
+1 2 3 4  | ELV | 1 2 3 4
---
+++ module1/part3/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+)
+
+var boat = "⛵"
+var river = "🌊"
+var human = "👨"
+
+var fox = "🦊"
+var chicken = "🐔"
+var corn = "🌽"
+
+type actor struct {
+	icon     string
+	name     string
+	position string
+	alive    bool
+}
+
+func initActor(icon_ string, name_ string) *actor {
+	newActor := actor{
+		icon:     icon_,
+		name:     name_,
+		position: "left",
+		alive:    true}
+	return &newActor
+}
+
+func printActorState(myActor *actor) string {
+	actorState := "The " + myActor.icon + " is currently at the " + myActor.position + " of the " + river + "."
+	return actorState
+}
+
+func crossRiver(transporter *actor, transported *actor) string {
+	msg := ""
+	if transported.alive == false {
+		msg = transported.icon + " is dead!"
+	} else {
+
+		if transporter.position == transported.position {
+			if transporter.position == "left" {
+				transporter.position = "right"
+				transported.position = "right"
+
+			} else {
+
+				transporter.position = "left"
+				transported.position = "left"
+			}
+
+			msg = transporter.icon + " transports " + transported.icon + " to " + transported.position
+		} else {
+			msg = transporter.icon + " is not at the same position as " + transported.icon
+		}
+	}
+	return msg
+}
+
+func putInBoat() {
+
+}
+
+func viewState() {
+
+}
+
+func eats(actors *[4]actor) bool {
+	if actors[0].position != actors[1].position && actors[1].position == actors[2].position {
+		actors[2].alive = false
+		fmt.Println(actors[1].icon, "eats", actors[2].icon)
+	}
+	if actors[0].position != actors[2].position && actors[2].position == actors[3].position {
+		actors[3].alive = false
+		fmt.Println(actors[2].icon, "eats", actors[3].icon)
+	}
+	return true
+}
+
+func main() {
+	human := initActor("👨", "Human")
+	fox := initActor("🦊", "Fox")
+	chicken := initActor("🐔", "Chicken")
+	corn := initActor("🌽", "Corn")
+	actors := [4]actor{*human, *fox, *chicken, *corn}
+
+	fmt.Println(crossRiver(&actors[0], &actors[2]))
+	eats(&actors)
+
+}
